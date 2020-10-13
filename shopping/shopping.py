e9469c0145bddb32faeb527605be51d1c1531038
emreisler <emre.isler.9@gmail.com>
2020-10-13 21:24:31 +0300

    Update shopping.py
    Useless comments are removed.

--- shopping/shopping.py
+++ shopping/shopping.py
@@ -65,38 +65,8 @@ def load_data(filename):
 
 
     loaded_data = (evidence_list, label_list)
-    #print(len(evidence_list),len(label_list),len(evidence_list[0]),len(evidence_list[750]))
-
 
     return loaded_data
-    """
-    Load shopping data from a CSV file `filename` and convert into a list of
-    evidence lists and a list of labels. Return a tuple (evidence, labels).
-
-    evidence should be a list of lists, where each list contains the
-    following values, in order:
-        - Administrative, an integer 0 OK
-        - Administrative_Duration, a floating point number 1 OK
-        - Informational, an integer 2 OK
-        - Informational_Duration, a floating point number 3 OK
-        - ProductRelated, an integer 4 OK
-        - ProductRelated_Duration, a floating point number 5 OK
-        - BounceRates, a floating point number 6 OK
-        - ExitRates, a floating point number 7 OK
-        - PageValues, a floating point number 8 OK
-        - SpecialDay, a floating point number 9 OK
-        - Month, an index from 0 (January) to 11 (December) 10 
-        - OperatingSystems, an integer 11
-        - Browser, an integer 12 
-        - Region, an integer 13
-        - TrafficType, an integer 14
-        - VisitorType, an integer 0 (not returning) or 1 (returning) 15 
-        - Weekend, an integer 0 (if false) or 1 (if true) 16
-
-    labels should be the corresponding list of labels, where each label
-    is 1 if Revenue is true, and 0 otherwise.
-    """
-
 
 
 def train_model(evidence, labels):
@@ -104,12 +74,6 @@ def train_model(evidence, labels):
     trainModel = KNeighborsClassifier(n_neighbors=1)
     fittedModel = trainModel.fit(evidence,labels)
     return fittedModel
-    """
-    Given a list of evidence lists and a list of labels, return a
-    fitted k-nearest neighbor model (k=1) trained on the data.
-    """
-
-
 
 def evaluate(labels, predictions):
     total_true = 0
@@ -130,23 +94,6 @@ def evaluate(labels, predictions):
     sensitivity = sensitivity/total_true
     specificty = specificty/total_false
     return sensitivity, specificty
-
-    """
-    Given a list of actual labels and a list of predicted labels,
-    return a tuple (sensitivity, specificty).
-
-    Assume each label is either a 1 (positive) or 0 (negative).
-
-    `sensitivity` should be a floating-point value from 0 to 1
-    representing the "true positive rate": the proportion of
-    actual positive labels that were accurately identified.
-
-    `specificity` should be a floating-point value from 0 to 1
-    representing the "true negative rate": the proportion of
-    actual negative labels that were accurately identified.
-    """
-
-
 
 if __name__ == "__main__":
     main()
